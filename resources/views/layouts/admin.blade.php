d4b2048ad0fce7153b402f956b9deb02b2306b44
shakil566 <shakilhossen566@gmail.com>
2022-10-08 21:30:20 +0600

    Project install, login, registration, admin panel setup

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>PhoneBook</title>
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <!-- CSS Files -->
+
+    <link href="{{ asset('admin/css/material-dashboard.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/css/custom.css') }}" rel="stylesheet">
+
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+</head>
+
+<body>
+    <div class="wrapper ">
+        @include('layouts.include.sidebar')
+        <div class="main-panel">
+            @include('layouts.include.navbar')
+
+            <div class="content">
+                @yield('content')
+            </div>
+            @include('layouts.include.footer')
+        </div>
+    </div>
+
+
+    <!--   Core JS Files   -->
+    <script src="{{ asset('admin/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/js/popper.min.js') }}"></script>
+    <script src="{{ asset('admin/js/bootstrap-material-design.min.js') }}"></script>
+    <script src="{{ asset('admin/js/perfect-scrollbar.jquery.min.js') }}"></script>
+
+</body>
+
+</html>
